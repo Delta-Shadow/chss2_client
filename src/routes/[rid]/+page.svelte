d15fa1f0f866cb9ab070296d1fceed06b1ba2701
DS <mailforgautamvg@gmail.com>
2023-06-27 19:17:14 +0530

    added textbox component

--- src/routes/[rid]/+page.svelte
+++ src/routes/[rid]/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 
+	// Components
+	import Textbox from './textbox.svelte'
+
 	// Stores
 	import server from '$lib/stores/server.js'
 	import session from '$lib/stores/session.js'
@@ -14,13 +17,15 @@
 	// Props
 	export let data // For receiving data from page.ts
 
+	// Variables to store state
+
 	onMount(() => {
-		server.connect()
-		let name = prompt('Enter nickname')
-		if (name == null) name = rand_name()
-		server.join(name, data.rid, err => {
-			console.log(err)
-		})
+		// server.connect()
+		// let name = prompt('Enter nickname')
+		// if (name == null) name = rand_name()
+		// server.join(name, data.rid, err => {
+		// console.log(err)
+		// })
 	})
 
 	$: {
@@ -31,6 +36,35 @@
 			game: $game
 		})
 	}
+
+	function send_msg(e: CustomEvent<{ text: string }>) {
+		console.log(e.detail.text)
+	}
 </script>
 
-<h1>Hello World!</h1>
+<!-- outer container -->
+<div class="flex flex-row items-stretch w-full h-full p-4">
+	<!-- main -->
+	<div class="w-2/3 lg:w-3/4">Main</div>
+
+	<!-- side -->
+	<div class="w-1/3 lg:w-1/4 border-4 border-green-600 p-2 flex flex-col">
+		<!-- messages area -->
+		<div class="flex-grow">
+			<ul>
+				<li><em>author: </em>content</li>
+				<li><em>author: </em>content</li>
+				<li><em>author: </em>content</li>
+			</ul>
+
+			<!-- input line -->
+			<div class="flex flex-row gap-2">
+				<span class="text-green-600">You: </span>
+				<Textbox on:submit={send_msg} />
+			</div>
+		</div>
+
+		<!-- player's list -->
+		<div class="h-8">Player's online: None</div>
+	</div>
+</div>
---
+++ src/routes/[rid]/textbox.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { createEventDispatcher, onMount } from 'svelte'
+
+	// Constants
+	const cursor = '_'
+	const blink_interval = 500
+
+	// Variables to store state
+	let text = ''
+	let cursor_visible = false
+	$: appendage = cursor_visible ? cursor : ''
+	$: display = text + appendage
+
+	// Element refs
+	let input_field: HTMLElement
+
+	// Event Dispatcher
+	const dispatch = createEventDispatcher<{ submit: { text: string } }>()
+
+	onMount(() => {
+		const timer = setInterval(toggle_cursor, blink_interval)
+		return () => clearInterval(timer)
+	})
+
+	function toggle_cursor() {
+		cursor_visible = !cursor_visible
+	}
+
+	function handle_submit() {
+		dispatch('submit', { text })
+	}
+
+	function focus() {
+		input_field.focus()
+	}
+</script>
+
+<form class="w-full flex-grow relative" on:submit={handle_submit}>
+	<p class="absolute w-full">{display}</p>
+	<input
+		autofocus
+		type="text"
+		class="absolute opacity-0 w-full"
+		bind:value={text}
+		bind:this={input_field}
+	/>
+</form>
