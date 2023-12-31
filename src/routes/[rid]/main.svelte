100432375498ec104385437537b0fc7aef15a1f9
DS <mailforgautamvg@gmail.com>
2023-12-31 11:55:47 +0530

    last commit

--- src/routes/[rid]/main.svelte
+++ src/routes/[rid]/main.svelte
@@ -1,1 +1,7 @@
-<div class="flex-grow border-4 border-green-800">Main</div>
+<script>
+	import Chessboard from './chessboard.svelte'
+</script>
+
+<div class="flex-grow border-4 border-green-800">
+	<Chessboard />
+</div>
